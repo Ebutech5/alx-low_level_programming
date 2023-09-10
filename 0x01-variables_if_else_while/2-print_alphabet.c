6d0e8dc85e5f7f9376971578e40741cc6b894daa
Ebukings5 <cristofarokoye@gmail.com>
2023-09-10 13:15:34 -0700

    follow function

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /*
-  main - Entry point
-  Return: Always 0 (Success)
+*  main - Entry point
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	for (m = 'a' ; m <= 'z' ; m++)
-		putchar(m);
+	for (i = 'a' ; i <= 'z' ; m++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }

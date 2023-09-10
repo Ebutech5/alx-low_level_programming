90c00ce9b4de6c4b8c0f21a38a8a2bcc206bc18f
Ebukings5 <cristofarokoye@gmail.com>
2023-09-10 13:12:42 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+  main - Entry point
+  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	for (m = 'a' ; m <= 'z' ; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}

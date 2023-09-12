78e7781c112e100d41c07afce89de359f7e4e721
Ebukings5 <cristofarokoye@gmail.com>
2023-09-12 16:40:26 -0700

    lower card

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* int _islower - print a function that checks lower case
+*@c: parameter checking a lower case
+* Return: 1 if positive and 0 if negative
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' ; && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

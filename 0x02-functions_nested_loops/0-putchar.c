a69c022eb62edcdd0a106b0527d545c16701320d
Ebukings5 <cristofarokoye@gmail.com>
2023-09-12 13:45:00 -0700

    code printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
-*main - function printing _putchar
-*Return: always 0 (success)
+* main - function that prints _putchar
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

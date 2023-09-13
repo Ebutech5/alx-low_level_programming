664d03fe194e720b4f525195b648b951d40cc1e9
Ebukings5 <cristofarokoye@gmail.com>
2023-09-13 14:39:47 -0700

    multiplication of number

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
-			sum = i + j;
+			sum = i * j;
 			if (j == 0)
-				printf("%d, ", sum);
+				printf("2%d, ", sum);
 			else
 			{
 				printf("%2d", sum);

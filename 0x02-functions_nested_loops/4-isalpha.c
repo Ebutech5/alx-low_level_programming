04cff0f53038722d632063739449f5f9e5db43f2
Ebukings5 <cristofarokoye@gmail.com>
2023-09-13 04:55:11 -0700

    Function printing alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - writes a function that checks alphabetic character
+* @c: function parameter
+* Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || ((c >= 'A' && <= 'Z'))
+			return (1);
+	else
+			return (0);
+}

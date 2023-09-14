a528c3f350b749b34db7c35c010d8d93dba5a0b5
Ebukings5 <cristofarokoye@gmail.com>
2023-09-14 15:41:32 -0700

    print a square using # character

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints square with '#' character
+ * @size: function parameter
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 1 ; j <= size ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}

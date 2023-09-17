bbcdba8381b9c2d6b46363f7f67af6f36f2a040c
Ebukings5 <cristofarokoye@gmail.com>
2023-09-17 13:09:06 -0700

    tripple digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (k = 0; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar('0' + i);
 				putchar('0' + j);

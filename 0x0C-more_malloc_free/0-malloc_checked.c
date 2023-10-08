5e0937e34dafd36ecc1223b45b784c903360d428
Ebukings5 <cristofarokoye@gmail.com>
2023-10-08 14:43:11 +0100

    locate a memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: int parameter to memory
+ *
+ * Return: 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+readme for this task 0x0C-more_malloc_free

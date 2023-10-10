352457dd46affe9ee9410c2e8eaa6215231cfe4a
Ebukings5 <cristofarokoye@gmail.com>
2023-10-10 14:27:23 +0100

    woof

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+a readme file at the root of my current directory 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - define a new type element
+ * @name: name of dog
+ * @age: pointer to the size of dog
+ * @owner: pointer to the name of owner
+ *
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

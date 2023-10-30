1288c053c60383701568ab0a5effa77e96457447
Ebukings5 <cristofarokoye@gmail.com>
2023-10-30 14:35:27 +0100

    Read only file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads and print a file to stdout
+ * @filename: The filename to read
+ * @letters: The number of letters to read and print
+ *
+ * Return: Actual number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	bytes_read = 0;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written < (size_t)bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (bytes_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+readme file for file input output

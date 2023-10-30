dc96ffe1fa9701762d2ca41d25f36ad17d4b0620
Ebukings5 <cristofarokoye@gmail.com>
2023-10-30 15:05:09 +0100

    Read only

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,11 +16,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
 		close(fd);
